08697e7a2fe868d107ecdf0ddde00d2b594335d4
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-28 10:34:46 -0700

    Removes password returned

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,7 @@ const logoutUser = (async (req, res) => {
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = (async (req, res) => {
-    const users = await User.find({});
+    const users = await User.find({}).select("-password");
 
     res.status(200).json(users);
 });
