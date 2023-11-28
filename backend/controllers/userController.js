9f1575a9490d92777b5fb8f8e46f57e6c1222fdb
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-28 10:31:21 -0700

    Validation & Error Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,30 +32,32 @@ const authUser = (async (req, res) => {
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const {name, email, password} = req.body;
+    try {
+        const {name, email, password} = req.body;
 
-    const userExist = await User.findOne({email});
+        const userExist = await User.findOne({email});
 
-    if (userExist) {
-        res.status(400);
-        throw new Error("User already exists");
-    }
+        if (userExist) {
+            res.status(400).json({message: "User already exists"});
+        }
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-    });
-
-    if (user) {
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
+        const user = await User.create({
+            name,
+            email,
+            password,
         });
-    } else {
-        res.status(400);
-        throw new Error("Invalid user data");
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            });
+        } else {
+            res.status(400).json({message: "Invalid user data"});
+        }
+    } catch (err) {
+        res.status(500).json({message: "Invalid user data"});
     }
 });
 
