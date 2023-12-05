38e3c745dab5a37c035fad049c01b868a17016d7
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-12-05 12:05:59 -0700

    Send date & fixed data sent

--- backend/models/quizModel.js
+++ backend/models/quizModel.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema({
     currentScore: {type: Number, required: true},
-    userId: {type: String, required: true}
+    userId: {type: String, required: true},
+    date: {type: Date, required: true},
 });
 
 const Quiz = mongoose.model("Quiz", quizSchema);
