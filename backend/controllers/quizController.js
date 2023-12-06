a78b555304ad041918a5864dc7eaadba2be19fb6
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-12-06 12:24:31 -0700

    Query all user quiz info

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -8,42 +8,57 @@ const getScores = (async (req, res) => {
     try {
         const answers = await Quiz.find({});
 
-        res.status(200).json(answers)
+        res.status(200).json(answers);
     } catch (err) {
-        console.error("Cant get scores")
+        console.error("Cant get scores");
     }
 });
 
 const postScores = (async (req, res) => {
     try {
         const {currentScore, userId} = req.body;
-        console.log(currentScore, "1")
-        console.log(userId, "userID")
-        const date = new Date()
+        console.log(currentScore, "1");
+        console.log(userId, "userID");
+        const date = new Date();
 
         // console.log(currentScore, "1")
 
         const quiz = await Quiz.create({
             currentScore,
             userId,
-            date
-        })
+            date,
+        });
 
-        console.log(quiz, "quiz")
+        console.log(quiz, "quiz");
 
         if (quiz) {
-            console.log(quiz)
+            console.log(quiz);
 
             res.status(201).json({
                 quizScores: quiz.quizScores,
-            })
+            });
         }
     } catch (err) {
-        console.error("Cant send scores")
+        console.error("Cant send scores");
+    }
+});
+
+const userSummary = (async (req, res) => {
+    try {
+        const {userId} = req.body;
+
+        const summary = await Quiz.find({userId: userId});
+        console.log(summary);
+
+        res.status(200).json({summary});
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({message: "Not found"});
     }
 });
 
 export {
     getScores,
-    postScores
+    postScores,
+    userSummary,
 };
